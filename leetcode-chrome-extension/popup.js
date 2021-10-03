3a4cbaebceea974c210190b82d4184ffff454c66
Ashish Kumar <ashish.888282@gmail.com>
2021-10-03 16:53:47 -0400

    CE v2

--- leetcode-chrome-extension/popup.js
+++ leetcode-chrome-extension/popup.js
@@ -1,20 +1,3 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
 
 // The body of this function will be executed as a content script inside the
 // current page
@@ -24,67 +7,6 @@ function setPageBackgroundColor() {
   });
 }
 
-
-// Lettcode
-// let signIn = document.getElementById("signin");
-
-// signIn.addEventListener("click", async () => {
-//   let leetCodeId = document.getElementById("leetcodeId").value;
-//   document.getElementById("leetCodeStats").innerHTML= `Hey, welcome ${leetCodeId}`; 
-//   const url = 'https://salty-waters-49462.herokuapp.com/leetcode.com/graphql';
-//   // const url = 'https://swapi-graphql.netlify.app/.netlify/functions/index';
-//   console.log('I was here: ' + url);
-//   fetch(url, {
-//     method: 'POST',
-//     headers: { 
-//       'Access-Control-Allow-Origin': '*',
-//       'Content-Type': 'application/json'
-//       // 'Access-Control-Allow-Credentials': 'true',
-//       // 'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'
-//     },
-//     body: JSON.stringify({
-//       query: `
-//         matchedUser($username: YOUR_USERNAME!) {
-//           username
-//           submitStats: submitStatsGlobal {
-//             acSubmissionNum {
-//             difficulty
-//             count
-//             submissions
-//             }
-//           }
-//         }
-//       `,
-//       variables: {
-//         username: 'akumar5'
-//       }
-//     })
-//   })
-//   .then((res) => res.json())
-//   .then((result) => {
-//     console.log(result);
-//     document.getElementById("test").innerHTML='chuchu is here'; 
-//   }).catch((err) => {
-//     document.getElementById("test").innerHTML='error chuchu is here, '+ err;
-//     console.log('error: ', err);
-//   });
-// });
-
-
-// Leetcode stats global
-// signIn.addEventListener("click", async () => {
-//   let leetCodeId = document.getElementById("leetcodeId").value;
-//   console.log('username: ' + leetCodeId);
-//   fetch(`https://salty-waters-49462.herokuapp.com/leetcode-stats-six.vercel.app/api?username=${leetCodeId}&theme=dark`, {'Access-Control-Allow-Origin': '*'})
-//       .then(response => response.text())
-//       .then(data => {
-//         console.log(data);
-//         document.getElementById("stats").innerHTML=data; 
-//       }).catch((err) => {
-//         document.getElementById("stats").innerHTML=err; 
-//         console.log('error: ', err);
-//       });
-// });
 
 //generate random question
 let getQuestion = document.getElementById("getQuestion");
@@ -96,8 +18,8 @@ getQuestion.addEventListener("click", async () => {
   myHeaders.append("Content-Type", "application/json");
 
   var graphql = JSON.stringify({
-    query: `query getTopicTag($slug: String!) {topicTag(slug: $slug){name translatedName questions{status title difficulty titleSlug }} }`,
-    variables: {"slug":`${topic}`}
+    query: `query getTopicTag($slug: String!) {topicTag(slug: $slug){name translatedName questions{status title difficulty titleSlug acRate}} }`,
+    variables: { "slug": `${topic}` }
   })
   var requestOptions = {
     method: 'POST',
@@ -110,53 +32,27 @@ getQuestion.addEventListener("click", async () => {
     .then(result => {
       var array = ["Easy", "Medium", "Hard"]
       let difficulty = document.querySelector('input[name="difficulty"]:checked').value;
-      if(difficulty === "Random"){
+      if (difficulty === "Random") {
         difficulty = array[Math.floor(Math.random() * array.length)];
       }
-      
+
       let questionsArray = result.data.topicTag.questions || [];
-      let filteredquestions = questionsArray.filter(item => item.difficulty===difficulty);
+      let filteredquestions = questionsArray.filter(item => item.difficulty === difficulty);
       let size = filteredquestions.length;
       let randomQuestion = Math.floor(Math.random() * size);
       var problemURL = `https://leetcode.com/problems/${filteredquestions[randomQuestion].titleSlug}`;
       document.getElementById("problemURL").setAttribute("href", problemURL);
       document.getElementById("problemURL").style.display = "";
-      document.getElementById("question").innerHTML=filteredquestions[randomQuestion].titleSlug;
-      document.getElementById("difficulty").innerHTML=difficulty;
+      document.getElementById("question").innerHTML = filteredquestions[randomQuestion].title;
+      document.getElementById("difficulty").innerHTML = difficulty;
+      // document.getElementById("acRate").innerHTML = filteredquestions[randomQuestion].acRate;
+      // document.getElementById("desc").style.visibility = "hidden";
     })
     .catch(error => {
-      document.getElementById("question").innerHTML=error; 
+      document.getElementById("question").innerHTML = error;
     });
 });
 
-console.clear();
-
-const loginBtn = document.getElementById('login');
-const signupBtn = document.getElementById('signup');
-
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
-
-signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
 
 // signUp Button
 const signupBtn1 = document.getElementById('submit-btn-signup');
@@ -166,27 +62,36 @@ signupBtn1.addEventListener('click', (e) => {
   var loginDiv = document.getElementById("loginDiv");
   signupDiv.classList.add("slide-up");
   loginDiv.classList.remove("slide-up");
+  document.getElementById("loginDiv").style.visibility = "visible";
+  document.getElementById("signupDiv").style.visibility = "hidden";
 
   // display the problem solved status
   let leetCodeId = document.getElementById("leetcodeId").value;
+  let username = leetCodeId;
+  chrome.storage.sync.set({ username });
   console.log('username: ' + leetCodeId);
-  fetch(`https://salty-waters-49462.herokuapp.com/leetcode-stats-six.vercel.app/api?username=${leetCodeId}&theme=dark`, {'Access-Control-Allow-Origin': '*'})
-      .then(response => response.text())
-      .then(data => {
-        console.log(data);
-        document.getElementById("stats").innerHTML=data; 
-      }).catch((err) => {
-        document.getElementById("stats").innerHTML=err; 
-        console.log('error: ', err);
+  fetch(`https://salty-waters-49462.herokuapp.com/leetcode-stats-six.vercel.app/api?username=${leetCodeId}&theme=dark`, { 'Access-Control-Allow-Origin': '*' })
+    .then(response => response.text())
+    .then(data => {
+      console.log(data);
+      chrome.storage.sync.get('username', ({ name }) => {
+        console.log('name: ', name)
+        document.getElementById('username').innerHTML = name;
       });
-  
-	// let parent = e.target.parentNode.parentNode;
-	// Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-	// 	if(element !== "slide-up") {
-	// 		parent.classList.add('slide-up')
-	// 	}else{
-	// 		signupBtn.parentNode.classList.add('slide-up')
-	// 		parent.classList.remove('slide-up')
-	// 	}
-	// });
+      document.getElementById("stats").innerHTML = data;
+      document.getElementById("getQuestion").click();
+    }).catch((err) => {
+      document.getElementById("stats").innerHTML = err;
+      console.log('error: ', err);
+    });
+
 });
+
+document.getElementById("login").addEventListener("click", goToLogin);
+
+function goToLogin(){
+  signupDiv.classList.remove("slide-up");
+  loginDiv.classList.add("slide-up");
+  document.getElementById("loginDiv").style.visibility = "hidden";
+  document.getElementById("signupDiv").style.visibility = "visible";
+}
