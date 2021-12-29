0e3be5957c71dd297857cc049b7d30d1ce5a8996
Ashish Kumar <ashish.888282@gmail.com>
2021-12-29 16:34:15 -0500

    Populating Next Right Pointers in Each Node

---
+++ Trees2/populating_next_right_pointers.py
@@ -0,0 +1,59 @@
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Example 2:
+
+Input: root = []
+Output: []
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+      
+      def helper(root):
+        if not root.left and not root.right: return
+        if not root.next:
+          root.left.next = root.right
+          root.right.next = None
+        else:
+          root.left.next = root.right
+          root.right.next = root.next.left
+        helper(root.left)
+        helper(root.right)
+        
+      if not root:
+        return
+      root.next = None
+      helper(root)
+      return root
+      
+          
+          
+          
+        
