f2ddeafb614b15a258ab2e5514730e06cc411c4a
Ashish Kumar <ashish.888282@gmail.com>
2022-01-05 21:20:55 -0500

    Car Pooling

---
+++ Arrays/car_polling.py
@@ -0,0 +1,35 @@
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trip[i] = [numPassengersi, fromi, toi]
+ indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively.
+  The locations are given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+
+Example 1:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+Output: false
+Example 2:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+Output: true
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        lst = []
+        
+        for p, start, end in trips:
+          lst.append((start, p))
+          lst.append((end, -p))
+        
+        lst.sort()
+        output = 0
+        
+        for i in lst:
+          output += i[1]
+          if output > capacity:
+            return False
+        
+        return True
