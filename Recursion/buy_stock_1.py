be3fccc27956303c74d63013fda3eab67ce06ecd
Ashish Kumar <ashish.888282@gmail.com>
2022-01-31 20:57:31 -0500

    buy & sell stock

--- Recursion/buy_stock_1.py
+++ Recursion/buy_stock_1.py
@@ -10,6 +10,7 @@
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 # Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
+#M1
 def buy_stock(prices):
   max_profit = 0
   buy = float('inf')
@@ -21,4 +22,24 @@ def buy_stock(prices):
 
   return max_profit
 
+#M2
+class Solution:
+    def maxProfit(prices):
+      if len(prices) <= 1:
+        return 0
+      if len(prices) == 2:
+        if prices[1] < prices[0]:
+          return 0
+        else:
+          return prices[1] - prices[0]
+      
+      profit = 0
+      min_value = float('inf')
+      for i in prices:
+        if i < min_value:
+          min_value = i
+        elif i - min_value > profit:
+          profit = i - min_value
+      
+      return profit
 print(buy_stock([7,1,5,3,6,4]))
